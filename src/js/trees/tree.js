77a91a1dcdf1b8b9a658e9504569df2a5ce73c56
Alejo <alejovillores@gmail.com>
2021-03-29 17:21:18 -0300

    clean

--- src/js/trees/tree.js
+++ src/js/trees/tree.js
@@ -10,50 +10,44 @@ export default class Tree {
 		return this.root != null;
 	}
 
-    addSmallerNode(root,newNode){
-        const rigthNode = root.getRigthNextNode();
-        
-        if (rigthNode === null){
-            root.setLeftNextNode(newNode);
-        }
-        else{
-            if (newNode.getData() >= rigthNode.getData()) {
-                this.addBiggerNode(rigthNode,newNode);
-            }
-            else{
-                this.addSmallerNode(rigthNode,newNode);
-            }
-        }
-        
-    }
+	addSmallerNode(root, newNode) {
+		const rigthNode = root.getRigthNextNode();
 
-    addBiggerNode(root,newNode){
-        const leftNode = root.getLeftNextNode();
-        
-        if (leftNode === null){
-            root.setLeftNextNode(newNode);
-        }
-        else{
-            if (newNode.getData() >= leftNode.getData()) {
-                this.addBiggerNode(leftNode,newNode);
-            }
-            else{
-                this.addSmallerNode(leftNode,newNode);
-            }
-        }
-        
-    }
+		if (rigthNode === null) {
+			root.setLeftNextNode(newNode);
+		} else {
+			if (newNode.getData() >= rigthNode.getData()) {
+				this.addBiggerNode(rigthNode, newNode);
+			} else {
+				this.addSmallerNode(rigthNode, newNode);
+			}
+		}
+	}
+
+	addBiggerNode(root, newNode) {
+		const leftNode = root.getLeftNextNode();
+
+		if (leftNode === null) {
+			root.setLeftNextNode(newNode);
+		} else {
+			if (newNode.getData() >= leftNode.getData()) {
+				this.addBiggerNode(leftNode, newNode);
+			} else {
+				this.addSmallerNode(leftNode, newNode);
+			}
+		}
+	}
 
 	addNode(value) {
-        const newNode = new TreeNode(vale);
+		const newNode = new TreeNode(vale);
 		if (this.isEmpty) {
-            this.root = newNode;
-		}else{
-            if (value >= this.root.getData()) {
-                this.addBiggerNode(this.root,newNode);
-            }else{
-                this.addSmallerNode(this.root,newNode);
-            }
-        }
+			this.root = newNode;
+		} else {
+			if (value >= this.root.getData()) {
+				this.addBiggerNode(this.root, newNode);
+			} else {
+				this.addSmallerNode(this.root, newNode);
+			}
+		}
 	}
 }
